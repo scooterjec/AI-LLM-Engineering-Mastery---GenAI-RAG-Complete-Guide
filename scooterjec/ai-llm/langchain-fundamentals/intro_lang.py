0fe32f657cb20ee78f2d6fb7e4a0231b707cd6b9
JUAN ESPINO CAMPOS <jespinca@gruposantander.com>
2026-02-06 14:09:14 +0100

    LangChain example

--- scooterjec/ai-llm/langchain-fundamentals/intro_lang.py
+++ scooterjec/ai-llm/langchain-fundamentals/intro_lang.py
@@ -1,10 +1,26 @@
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 
 from langchain.chat_models import init_chat_model
-model = init_chat_model("gpt-4o-mini", model_provider = "openai")
+# model = init_chat_model("gpt-4o-mini", model_provider = "openai")
+model = init_chat_model("llama3.2", model_provider = "ollama")
 
-response = model.invoke("Hello how are you?")
-print(response)
+
+#response = model.invoke("Hello how are you?")
+#print(response)
+
+from langchain_core.prompts import ChatPromptTemplate
+
+system_template = "Translate the following from English into {language}"
+prompt_template = ChatPromptTemplate.from_messages(
+        [("system", system_template), ("user", "{text}")]
+    )
+prompt = prompt_template.invoke({"language": "Italian", "text": "Hi!"})
+
+print(prompt)
+
+response = model.invoke(prompt)
+print(response.content)
+
